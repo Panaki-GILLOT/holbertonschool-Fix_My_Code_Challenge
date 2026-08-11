24ff700088210acf13427fb4a5e5f9e09f68c5f4
panaki-gillot <gillotpanaki@gmail.com>
2026-08-11 10:15:04 +0200

    Fix input validation in print_square to remove non-positive size check

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -2,7 +2,7 @@
 
 const size = parseInt(process.argv[2], 10);
 
-if (!process.argv[2] || isNaN(size) || size <= 0) {
+if (!process.argv[2] || isNaN(size)) {
   console.log('Missing size');
 } else {
   for (let i = 0; i < size; i++) {
